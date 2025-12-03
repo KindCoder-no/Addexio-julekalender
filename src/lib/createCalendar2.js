bdbbccfa96f8d509152a31c0902515cbffac5e35
Emre Sanden <40148297+KindCoder-no@users.noreply.github.com>
2025-12-03 12:53:48 +0100

    Update YouTube IDs in createCalendar2.js

--- src/lib/createCalendar2.js
+++ src/lib/createCalendar2.js
@@ -9,139 +9,139 @@ export const calendarData = [
     {
       id: "hatch-2",
       nr: 2,
-      youtubeId: "VIDEO_ID_2",
+      youtubeId: "GzCGTq8DiOc",
       open: false
     },
     {
       id: "hatch-3",
       nr: 3,
-      youtubeId: "VIDEO_ID_3",
+      youtubeId: "aVZnAqbgrRI",
       open: false
     },
     {
       id: "hatch-4",
       nr: 4,
-      youtubeId: "VIDEO_ID_4",
+      youtubeId: "EJiXRA5xDys",
       open: false
     },
     {
       id: "hatch-5",
       nr: 5,
-      youtubeId: "VIDEO_ID_5",
+      youtubeId: "MgsC2jpp8ys",
       open: false
     },
     {
       id: "hatch-6",
       nr: 6,
-      youtubeId: "VIDEO_ID_6",
+      youtubeId: "sMMBopNPFaQ",
       open: false
     },
     {
       id: "hatch-7",
       nr: 7,
-      youtubeId: "VIDEO_ID_7",
+      youtubeId: "ZIkUP6DQ9lI",
       open: false
     },
     {
       id: "hatch-8",
       nr: 8,
-      youtubeId: "VIDEO_ID_8",
+      youtubeId: "kYcIIzXbQXQ",
       open: false
     },
     {
       id: "hatch-9",
       nr: 9,
-      youtubeId: "VIDEO_ID_9",
+      youtubeId: "ph0jYJyJKxA",
       open: false
     },
     {
       id: "hatch-10",
       nr: 10,
-      youtubeId: "VIDEO_ID_10",
+      youtubeId: "y0oWaZqd0sk",
       open: false
     },
     {
       id: "hatch-11",
       nr: 11,
-      youtubeId: "VIDEO_ID_11",
+      youtubeId: "a4ZFEH9X1YY",
       open: false
     },
     {
       id: "hatch-12",
       nr: 12,
-      youtubeId: "VIDEO_ID_12",
+      youtubeId: "ccIXOWh7aYM",
       open: false
     },
     {
       id: "hatch-13",
       nr: 13,
-      youtubeId: "VIDEO_ID_13",
+      youtubeId: "tYHO0kBUvKI",
       open: false
     },
     {
       id: "hatch-14",
       nr: 14,
-      youtubeId: "VIDEO_ID_14",
+      youtubeId: "bvZroMX9Huc",
       open: false
     },
     {
       id: "hatch-15",
       nr: 15,
-      youtubeId: "VIDEO_ID_15",
+      youtubeId: "hLtpDX-9NUc",
       open: false
     },
     {
       id: "hatch-16",
       nr: 16,
-      youtubeId: "VIDEO_ID_16",
+      youtubeId: "R1W8fWxLI88",
       open: false
     },
     {
       id: "hatch-17",
       nr: 17,
-      youtubeId: "VIDEO_ID_17",
+      youtubeId: "54EGtUf8cMs",
       open: false
     },
     {
       id: "hatch-18",
       nr: 18,
-      youtubeId: "VIDEO_ID_18",
+      youtubeId: "dqPR-5v4tYo",
       open: false
     },
     {
       id: "hatch-19",
       nr: 19,
-      youtubeId: "VIDEO_ID_19",
+      youtubeId: "i0IElCz7WQ4",
       open: false
     },
     {
       id: "hatch-20",
       nr: 20,
-      youtubeId: "VIDEO_ID_20",
+      youtubeId: "Y_sIBA8KpDs",
       open: false
     },
     {
       id: "hatch-21",
       nr: 21,
-      youtubeId: "VIDEO_ID_21",
+      youtubeId: "QTPoMtQcUd8",
       open: false
     },
     {
       id: "hatch-22",
       nr: 22,
-      youtubeId: "VIDEO_ID_22",
+      youtubeId: "z-ZPYWdOJyc",
       open: false
     },
     {
       id: "hatch-23",
       nr: 23,
-      youtubeId: "VIDEO_ID_23",
+      youtubeId: "rZ3gKnjj7LU",
       open: false
     },
     {
       id: "hatch-24",
       nr: 24,
-      youtubeId: "VIDEO_ID_24",
+      youtubeId: "1ncHxcS322Y",
       open: false
     },
 ];
